5d9060bc567df3c8365cb83c6f1ba0dcf90d7f76
priyaaa80 <priyashukla282006@gmail.com>
2025-08-22 08:58:00 +0530

    Initial commit - Food Booking Website

---
+++ script.js
@@ -0,0 +1,25 @@
+// header scroll
+
+//nav hide
+
+//swiper slider
+ var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    speed: 1000,
+  });
+
+//counter design
+
+//slider
